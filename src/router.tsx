8bc3e83f7c365d984ce876a0fc67dde505eeaf01
khahhy <127372724+khahhy@users.noreply.github.com>
2025-11-01 19:30:24 +0700

    fix(deploy): add domain name in router

--- src/router.tsx
+++ src/router.tsx
@@ -3,14 +3,19 @@ import App from "@/App";
 import Dashboard from "@/containers/Dashboard/Dashboard";
 import ImageDetail from "@/containers/ImageDetail/ImageDetail";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        { index: true, element: <Dashboard /> },
+        { path: "photos", element: <Dashboard /> },
+        { path: "photos/:id", element: <ImageDetail /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "photos", element: <Dashboard /> },
-      { path: "photos/:id", element: <ImageDetail /> },
-    ],
-  },
-]);
+    basename: "/AdvWeb-IA02",
+  }
+);
